0b9f03f32359aaaeb5daec44bc03648e431a2609
student <volzhenin.egor@gmail.com>
2026-03-25 11:26:19 +0300

    5

--- src/config/routes.php
+++ src/config/routes.php
@@ -3,6 +3,7 @@
 return[
   '~^articles/$~' => [\src\controllers\ArticlesController::class,'index'], 
   '~^articles/article/(\d+)$~' => [\src\controllers\ArticlesController::class,'view'],
+  '~^articles/article/(\d+)/edit$~' => [\src\controllers\ArticlesController::class,'edit'],
  '~^$~' => [\src\controllers\MainController::class,'main'], 
 
 ]; 
--- src/controllers/ArticlesController.php
+++ src/controllers/ArticlesController.php
@@ -10,28 +10,25 @@ class ArticlesController extends Controller
 {
     public function index()
     {
-        // $db = new Db();
         $articles = Articles::findAll();
-    //    $db->query('SELECT * FROM `articles`;' ,[], Articles::class);
         $this->view->renderHtml('articles/index.php', ['articles' => $articles]);
     }
     public function view($id)
     {
-        // $article = Articles::getById($id);
-       
-        // if($article !== null){
-        //      $author = Users::getById($article->getAuthorId());
-        //   $this->view->renderHtml('articles/view.php', ['article' => $article, 'author' =>$author]);
-        // }else{
-        //       $this->view->renderHtml('errors/404.php', [], 404); 
-        // }
         $article = Articles::getById($id);
         if($article !== null){
           $this->view->renderHtml('articles/view.php', ['article' => $article]);
         }else{
               $this->view->renderHtml('errors/404.php', [], 404); 
         }
-       
-       
+    }
+    public function edit($id)
+    {
+        $article = Articles::getById($id);
+        if($article === null){
+            $this->view->renderHtml('errors/404.php',[], 404);
+        }
+
+        var_dump($article);
     }
 }
